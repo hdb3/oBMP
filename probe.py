676333454a23a6325176b4198c68c6b34a5b32d7
Nicholas Hart <nic@hdb3.net>
2018-02-27 16:16:38 +0000

    support wildcards and patterns for the topic fields

--- probe.py
+++ probe.py
@@ -4,25 +4,25 @@ import sys
 
 from kafka import KafkaConsumer
 # kafka library installed via 'pip install kafka-python'
-# also require snappy: 'pip install python-snappy'
+# also requires snappy: 'pip install python-snappy'
 
 def listen(name,consumer,topics):
-    consumer.subscribe(topics=topics)
+    if (topics[0] in ('*','all')):
+        consumer.subscribe(pattern='.*')
+    else:
+        try:
+            consumer.subscribe(topics=topics)
+        except ValueError:
+            consumer.subscribe(pattern=topics[0])
     print('listening to',name, 'for topics',topics)
     for message in consumer:
-        # message value and key are raw bytes -- decode if necessary!
-        # e.g., for unicode: `message.value.decode('utf-8')`
-        print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+        print ("%s:%d:%d: key=%s length=%d" % (message.topic, message.partition, message.offset, message.key, len(message.value)))
 
 
-# refer to https://bgpstream.caida.org/v2-beta#bmp for the link and description of this service
 collectors =  [ ('caida openBMP',lambda: KafkaConsumer(bootstrap_servers=['bmp.bgpstream.caida.org:9092'],client_id='lancaster_university_UK',group_id='beta-bmp-stream')),
+                 # refer to https://bgpstream.caida.org/v2-beta#bmp for the link and description of this service
                 ('local openBMP',lambda: KafkaConsumer(bootstrap_servers=['r720:9092']))
               ]
-
-#caidaCollector = ('caida openBMP',KafkaConsumer(bootstrap_servers=['bmp.bgpstream.caida.org:9092'],client_id='lancaster_university_UK',group_id='beta-bmp-stream'))
-
-#localCollector = ('local openBMP',KafkaConsumer(bootstrap_servers=['r720:9092']))
 
 def probe(name,consumer):
     print('probing',name, 'for topics')
