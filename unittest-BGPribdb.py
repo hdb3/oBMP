9e28722448512a76bb5f279787ea4862f5f2cde5
nic hart <nic@hdb3.net>
2018-04-04 14:52:04 +0100

    pass simple unit tests(2) - need to write code to drop obsolete updates

--- unittest-BGPribdb.py
+++ unittest-BGPribdb.py
@@ -12,7 +12,7 @@ def make_test_paths():
     global test_prefixes
     test_prefixes=[]
     test_paths=[]
-    for i in range(10):
+    for i in range(100):
         path = Object()
         path.a = random.randint(0,0xffffffff)
         path.b = random.randint(0,0xffffffff)
@@ -42,21 +42,35 @@ rib = BGPribdb.BGPribdb()
 
 print (rib)
 
-for i in range(100):
+for i in range(10000):
     path = get_test_path()
     prefixes = []
     for j in range(random.randint(1,10)):
         prefixes.append(get_new_test_prefix())
+    rib.update(path,prefixes)
 
+for i in range(10000):
+    path = get_test_path()
+    prefixes = []
+    for j in range(random.randint(1,10)):
+        prefixes.append(get_old_test_prefix())
     rib.update(path,prefixes)
 
 print (rib)
 
 update=rib.get_update_request()
+update_count = 0
+update_pfx_count = 0
 while update:
     print(update)
     update=rib.get_update_request()
+    if update:
+        (path,pfxs)=update
+        update_count += 1
+        update_pfx_count += len(pfxs)
 
+print("update_count %d" % update_count)
+print("update_pfx_count %d" % update_pfx_count)
 print (rib)
 
 
