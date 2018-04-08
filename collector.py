83f3131cf56baf04c1959356d1f6d157f17b0bb0
Nicholas Hart <nic@hdb3.net>
2018-04-08 10:37:52 +0100

    first running version of collector.py

--- collector.py
+++ collector.py
@@ -4,7 +4,7 @@
 # collector.py
 # this is a generic TCP active sender
 #
-
+import os.path
 import yaml
 import sys
 import socket
@@ -25,7 +25,7 @@ class Session(threading.Thread):
             self.send( bytes("Hello caller! (%d)" % i,"ascii"))
             msg = self.recv()
             if msg:
-                print("msg rcv: %s" % hex(msg)
+                print("msg rcv: %s" % hex(msg))
             else:
                 test_msg = bytes("Hello world (%d)" % i,"ascii")
             self.send( bytes("Hello world! (%d)" % i,"ascii"))
@@ -40,38 +40,46 @@ Retrying = 5
 Connected = 6
 Disconnected = 7
 
+def log(c):
+    sys.stderr.write(c)
+    sys.stderr.flush()
+
 class Collector(threading.Thread):
 
-    def __init__(self,host,port,passive=False):
+    def __init__(self,name,host,port,passive=False):
         threading.Thread.__init__(self)
+        self.name = name
+        self.passive = passive
         self.daemon = False # keep main prog until collectors exit
         self.state = Initialising
         self.connections = 0
         self.event = threading.Event()
         self.address = (host,port)
+        self.address_name = "%s:%d" % (host,port)
+        self.log_err = lambda s : log("%s: %s\n" % (self.name,s))
 
     def run(self):
         while True:
             self.state = Connecting
             if self.passive:
                 if self.connections == 0:
-                    sys.stderr.write("awaiting connection to %s:%d\n" % self.address)
+                    self.log_err("awaiting connection on %s\n" % self.address_name)
                 else:
-                    sys.stderr.write("reawaiting connection to %s:%d\n" % self.address)
+                    self.log_err("reawaiting connection on %s\n" % self.address_name)
             else:
                 if self.connections == 0:
-                    sys.stderr.write("attempting connection to %s:%d\n" % self.address)
+                    self.log_err("attempting connection to %s\n" % self.address_name)
                 else:
-                    sys.stderr.write("reattempting connection to %s:%d\n" % self.address)
+                    self.log_err("reattempting connection to %s\n" % self.address_name)
             while self.state != Connected:
                 if self.passive:
                     try:
-                        socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        s.bind(self.address)
-                        s.listen(1)
-                        self.sock,self.address = s.accept()
+                        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        listen_socket.bind(self.address)
+                        listen_socket.listen(1)
+                        self.sock,self.address = listen_socket.accept()
                     except (socket.herror,socket.gaierror) as e:
-                        sys.stderr.write("unrecoverable error %s" % e + " connecting to %s:%d\n" % self.address)
+                        self.log_err("unrecoverable error %s" % e + " connecting to %s\n" % self.address_name)
                         self.state = Error
                         sys.exit()
                     except (socket.error,socket.timeout) as e:
@@ -80,13 +88,13 @@ class Collector(threading.Thread):
                         sleep(1)
                         continue
                     except Exception as e:
-                        sys.stderr.write("unknown error %s" % e + " connecting to %s:%d\n" % self.address)
+                        self.log_err("unknown error %s" % e + " connecting to %s\n" % self.address_name)
                         self.state = Error
                         sys.exit()
                     else:
                         self.state = Connected
                         self.connections += 1
-                        sys.stderr.write("connected to %s:%d\n" % self.address)
+                        self.log_err("connected to %s\n" % self.address_name)
                         self.event.clear()
                         self.event.wait()
                         self.event.clear()
@@ -94,11 +102,10 @@ class Collector(threading.Thread):
                         session.run(self.send,self.recv)
 
                 else:
-                    if self.connections == 0:
                     try:
                         self.sock = socket.create_connection(self.address,1)
                     except (socket.herror,socket.gaierror) as e:
-                        sys.stderr.write("unrecoverable error %s" % e + " connecting to %s:%d\n" % self.address)
+                        self.log_err("unrecoverable error %s" % e + " connecting to %s\n" % self.address_name)
                         self.state = Error
                         sys.exit()
                     except (socket.error,socket.timeout) as e:
@@ -107,13 +114,13 @@ class Collector(threading.Thread):
                         sleep(1)
                         continue
                     except Exception as e:
-                        sys.stderr.write("unknown error %s" % e + " connecting to %s:%d\n" % self.address)
+                        self.log_err("unknown error %s" % e + " connecting to %s\n" % self.address_name)
                         self.state = Error
                         sys.exit()
                     else:
                         self.state = Connected
                         self.connections += 1
-                        sys.stderr.write("connected to %s:%d\n" % self.address)
+                        self.log_err("connected to %s\n" % self.address_name)
                         self.event.clear()
                         self.event.wait()
                         self.event.clear()
@@ -123,15 +130,14 @@ class Collector(threading.Thread):
 
     def recv(self):
         if self.state != Connected:
-            sys.stderr.write('r')
-            sys.stderr.flush()
+            log('r')
             return None
         else:
             try:
                 msg = self.sock.recv(BUFSIZ)
                 fullmsg = msg
                 while BUFSIZ == len(msg):
-                    sys.stderr.write("\nget more bytes on recv\n")
+                    self.log_err("\nget more bytes on recv\n")
                     msg = self.sock.recv(BUFSIZ, socket.MSG_DONTWAIT)
                     fullmsg += msg
                 return fullmsg
@@ -139,26 +145,23 @@ class Collector(threading.Thread):
             except socket.error as errmsg:
                 if self.is_alive():
                     self.state = Disconnected
-                    sys.stderr.write('!')
-                    sys.stderr.flush()
+                    log('!')
                     self.event.set()
                     return
                 else:
-                    sys.stderr.write("socket manager has exited\n")
+                    self.log_err("socket manager has exited\n")
                     self.state = Error
                     sys.exit()
             except Exception as e:
-                sys.stderr.write("\nunknown error on recv %s\n" % e)
+                self.log_err("\nunknown error on recv %s\n" % e)
                 self.state = Error
                 return None
 
-            sys.stderr.write('+')
-            sys.stderr.flush()
+            log('+')
 
     def send(self,msg):
         if self.state != Connected:
-            sys.stderr.write('-')
-            sys.stderr.flush()
+            log('s')
         else:
             try:
                 self.sock.sendall(msg)
@@ -166,45 +169,53 @@ class Collector(threading.Thread):
             except socket.error as errmsg:
                 if self.is_alive():
                     self.state = Disconnected
-                    sys.stderr.write('!')
-                    sys.stderr.flush()
+                    log('s')
                     self.event.set()
                     return
                 else:
-                    sys.stderr.write("socket manager has exited\n")
+                    self.log_err("socket manager has exited\n")
                     self.state = Error
                     sys.exit()
-            sys.stderr.write('+')
-            sys.stderr.flush()
+            log('S')
 
-def main():
+def main(name,config):
 
-    with open("collector.yml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
-        if ('collector' not in cfg):
-            sys.exit("could not find section 'collector' in config file")
-        else:
-            forward_cfg=cfg['collector']
+    if ('collector' not in cfg):
+        sys.exit("could not find section 'collector' in config file")
+    else:
+        forward_cfg=cfg['collector']
 
-        if ('listener' not in forward_cfg and 'targets' not in forward_cfg):
-            sys.exit("could not find sub-section 'targets' or 'listener' in config file")
+    if ('listener' not in forward_cfg and 'targets' not in forward_cfg):
+        sys.exit("could not find sub-section 'targets' or 'listener' in config file")
 
-        collectors = []
-        if ('targets' in forward_cfg):
-            cfg_targets = forward_cfg['targets']
-            for target in cfg_targets:
-                assert 'host' in target
-                assert 'port' in target
-            collectors.append() = Collector(target['host'],target['port'],passive=False)
+    collectors = []
+    if ('targets' in forward_cfg):
+        cfg_targets = forward_cfg['targets']
+        for target in cfg_targets:
+            assert 'host' in target
+            assert 'port' in target
+            collectors.append(Collector(name,target['host'],target['port'],passive=False))
 
-        if ('listener' in forward_cfg):
-            cfg_listener = forward_cfg['listener']
-            for target in cfg_targets:
-                assert 'host' in cfg_listener
-                assert 'port' in cfg_listener
-            collectors.append() = Collector(cfg_listener['host'],cfg_listener['port'],passive=True)
+    if ('listener' in forward_cfg):
+        cfg_listener = forward_cfg['listener']
+        if '*' == cfg_listener['host']:
+            cfg_listener['host'] = ''
+        collectors.append(Collector(name,cfg_listener['host'],cfg_listener['port'],passive=True))
 
     for collector in collectors:
         collector.start()
 
-main()
+if len(sys.argv) > 1:
+    filenames = [sys.argv[1],'collector.yml']
+else:
+    filenames = ['collector.yml']
+for filename in filenames:
+    try:
+        cfg = yaml.load(open(filename, 'r'))
+    except Exception as e:
+        print("couldn't open %s as YAML config" % filename,file=sys.stderr)
+        print("exception %s" % e,file=sys.stderr)
+    else:
+        name = os.path.splitext(os.path.basename(filename))[0]
+        main(name,cfg)
+        exit()
