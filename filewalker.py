d995eaf20c8a351a312a578f6325473fbc7bfabf
nic hart <nic@nic-Precision-Tower-3420>
2018-04-10 18:20:02 +0100

    split filewalker.py into filewalker.py and bmpapp.py

--- filewalker.py
+++ filewalker.py
@@ -9,6 +9,7 @@ import bgpparse
 import BGPribdb
 import pprint
 import capabilitycodes
+import bmpapp
 
 
 logfile=sys.stdout
@@ -16,51 +17,6 @@ def eprint(s):
     logfile.write(s+'\n')
     logfile.flush()
     return
-
-class BmpContext:
-
-    def __init__(self):
-        self.rib = BGPribdb.BGPribdb()
-
-    def parse(self,msg):
-        if msg.msg_type == bmpparse.BMP_Initiation_Message:
-            print("-- BMP Initiation Message rcvd, length %d" % msg.length)
-            ##print(msg)
-        elif msg.msg_type == bmpparse.BMP_Peer_Up_Notification:
-            print("-- BMP Peer Up rcvd, length %d" % msg.length)
-            ##print(msg)
-            sent_open = bgpparse.BGP_message(msg.bmp_peer_up_sent_open)
-            print("capability codes in sent_open")
-            ##pprint.pprint(sent_open.bgp_open_optional_parameters)
-            for p in sent_open.bgp_open_optional_parameters:
-                if p in capabilitycodes.BGP_optional_capability_code_strings:
-                    print("(%d) : %s" % (p,capabilitycodes.BGP_optional_capability_code_strings[p]))
-                else:
-                    print ("(%d) : unknown capability code" % p)
-
-            ##print(sent_open)
-            rcvd_open = bgpparse.BGP_message(msg.bmp_peer_up_rcvd_open)
-            print("capability codes in rcvd_open")
-            for p in rcvd_open.bgp_open_optional_parameters:
-                if p in capabilitycodes.BGP_optional_capability_code_strings:
-                    print("(%d) : %s" % (p,capabilitycodes.BGP_optional_capability_code_strings[p]))
-                else:
-                    print ("(%d) : unknown capability code" % p)
-
-            ##pprint.pprint(rcvd_open.bgp_open_optional_parameters)
-            ##print(rcvd_open)
-        elif msg.msg_type == bmpparse.BMP_Statistics_Report:
-            print("-- BMP stats report rcvd, length %d" % msg.length)
-        elif msg.msg_type == bmpparse.BMP_Route_Monitoring:
-            #print("-- BMP Route Monitoring rcvd, length %d" % msg.length)
-            parsed_bgp_message = bgpparse.BGP_message(msg.bmp_RM_bgp_message)
-            self.rib.withdraw(parsed_bgp_message.withdrawn_prefixes)
-            if parsed_bgp_message.except_flag:
-                eprint("except during parsing at message no %d" % n)
-            else:
-                self.rib.update(parsed_bgp_message.attribute,parsed_bgp_message.prefixes)
-        else:
-            eprint("-- BMP non RM rcvd, BmP msg type was %d, length %d\n" % (msg.msg_type,msg.length))
 
 if len(sys.argv) > 1:
     filename = sys.argv[1]
@@ -79,7 +35,7 @@ else:
 
 n=0
 with open(filename,'rb') as f:
-    parser = BmpContext()
+    parser = bmpapp.BmpContext()
     filebuffer = bytearray(f.read(bufsiz))
     r=1
     while True:
