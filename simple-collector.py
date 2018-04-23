cbd48db04fd43734cd2bdd50029fbfb04d50c099
nh <nic@r720>
2018-04-23 14:28:14 +0100

    simple-collector.py is now BMP free, bmpcontext.py does all BMP

--- simple-collector.py
+++ simple-collector.py
@@ -6,29 +6,7 @@ from kafka import KafkaConsumer
 from kafka.errors import NoBrokersAvailable
 from oBMPparse import oBMP_parse
 
-import bmpapp
-from bmpparse import BMP_message, BMP_Initiation_Message, BMP_Termination_Message, BMP_Route_Monitoring, BMP_Statistics_Report
-class BMP_process:
-    
-    def __init__(self):
-        self.parser = bmpapp.BmpContext("simple")
-        self.peers = set()
-
-    def process_message(self,bmp_msg):
-        ( msg_type, peer, msg ) = self.parser.parse(BMP_message(bmp_msg))
-        if msg_type == BMP_Initiation_Message:
-            print("BMP session start")
-        elif msg_type == BMP_Termination_Message:
-            print("BMP session end")
-        else:
-            if not peer['hash'] in self.peers:
-                self.peers.add(peer['hash'])
-                print("new peer connected AS%d:%s" % (peer['remote_AS'], peer['remote_IPv4_address']))
-            if msg_type == BMP_Statistics_Report:
-                print("stats report for AS%d:%s" % (peer['remote_AS'], peer['remote_IPv4_address']))
-            elif msg_type == BMP_Route_Monitoring:
-                pass
-
+from bmpcontext import BMP_process
 
 if len(sys.argv) > 1:
     config_file = sys.argv[1]
@@ -53,19 +31,9 @@ bmp_process = BMP_process()
 try:
     for message in consumer:
         bmp_stream = oBMP_parse(bytearray(message.value))
-        bmp_stream, bmp_msg = BMP_message.get_next(bmp_stream)
-        while bmp_msg:
-            bmp_process.process_message(bmp_msg)
-            bmp_stream, bmp_msg = BMP_message.get_next(bmp_stream)
+        bmp_stream = bmp_process.get_next(bmp_stream)
+        while bmp_stream:
+            bmp_stream = bmp_process.get_next(bmp_stream)
 except KeyboardInterrupt:
     print("exit on keybaord interrupt")
 consumer.close()
-
-
-
-##from ipaddress import IPv4Address
-
-
-##remote_peer_address = IPv4Address(config['remote peer address'])
-
-
